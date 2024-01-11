2892419b9dbaf757b49b4225edbef329b4c55bbf
frontend200tb <frontend200tb@mail.ru>
2024-01-11 15:57:40 +0300

    feat: 12 getTail

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -228,8 +228,11 @@ function getHead(arr, n) {
  *    getTail([ 'a', 'b', 'c', 'd'], 3) => [ 'b', 'c', 'd' ]
  *    getTail([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  if (n) {
+    return arr.slice(-n);
+  }
+  return [];
 }
 
 /**
