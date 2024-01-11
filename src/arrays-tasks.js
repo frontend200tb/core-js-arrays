9c182bdb69239c1a6b692f92b3339b2ec3317883
frontend200tb <frontend200tb@mail.ru>
2024-01-11 14:38:30 +0300

    feat: 7 getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -143,8 +143,14 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  const sum = arr.reduce((acc, elem) => {
+    return acc + elem;
+  });
+  return Math.round((sum * 100) / arr.length) / 100;
 }
 
 /**
