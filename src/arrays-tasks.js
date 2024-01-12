869cdd7e018228d852941b6758cdceb9b42f13f8
frontend200tb <frontend200tb@mail.ru>
2024-01-12 14:10:30 +0300

    feat: 25 getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -460,8 +460,15 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers
+    .map((elem, ind) => {
+      if (!(elem % 2)) {
+        return -1;
+      }
+      return ind;
+    })
+    .filter((elem) => elem !== -1);
 }
 
 /**
